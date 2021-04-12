a17b6e7697d17a462cbc7060872b74615aaa0459
Athiley Luciano <athileyluciano@gmail.com>
2021-04-12 18:34:46 +0200

    adding seventh model of simple to do react - User Accounts

--- simple-todos-react/client/main.jsx
+++ simple-todos-react/client/main.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { TasksCollection } from '/imports/api/TasksCollection';
 import { render } from 'react-dom';
 import { App } from '/imports/ui/App';
 
+const SEED_USERNAME = 'meteorite';
+const SEED_PASSWORD = 'password';
+
 Meteor.startup(() => {
+  if (Meteor.isServer && !Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
   render(<App/>, document.getElementById('react-target'));
 });
